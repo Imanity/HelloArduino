2e269076d55b672b2b600c16eb813bec24165bb6
Li Zhaoyang <lizy14@yeah.net>
2016-01-19 12:45:52 +0000

    一块arduino连接两个传感器

---
+++ Sensor2Bluetooth/Sensor.h
@@ -0,0 +1,108 @@
+/*
+
+By Zhaoyang, Jan 2016.
+
+*/
+
+
+class Sensor{
+public:
+    int ID;
+    RTIMU *imu;                      // the IMU object
+    RTFusionRTQF fusion;             // the fusion object
+    RTIMUSettings settings;          // the settings object
+
+    void init(int ID_ = 0){
+        #ifdef VERBOSE
+            Serial.print("Initializing sensor #");
+            Serial.println(ID_);
+        #endif
+        
+        ID = ID_;
+        settings.m_I2CSlaveAddress = 0x68;
+        if(ID%2 == 1)
+            settings.m_I2CSlaveAddress = 0x69; 
+        
+        //dirty fix: lowers default sample rate
+        settings.m_MPU9250GyroAccelSampleRate = 60; /*MAGIC*/
+        settings.m_MPU9250CompassSampleRate = 30; /*MAGIC*/
+        
+        imu = RTIMU::createIMU(&settings);// create the imu object
+        
+        int errcode = imu->IMUInit();
+        
+        #ifdef VERBOSE
+            Serial.print("ArduinoIMU starting using device "); Serial.println(imu->IMUName());
+            if (errcode < 0) {
+                Serial.print("Failed to init IMU: "); Serial.println(errcode);
+            }
+            if (imu->getCalibrationValid())
+                Serial.println("Using compass calibration");
+            else
+                Serial.println("No valid compass calibration data");
+        #endif
+
+        // use of sensors in the fusion algorithm can be controlled here
+        // change any of these to false to disable that sensor
+
+        fusion.setGyroEnable(false);
+        fusion.setAccelEnable(true);
+        fusion.setCompassEnable(true);
+    }
+    int refresh(){
+        if (imu->IMURead()) { // get the latest data if ready yet
+            fusion.newIMUData(imu->getGyro(), imu->getAccel(), imu->getCompass(), imu->getTimestamp());
+        }
+    }
+    void sendToSerial(){
+
+      RTVector3 vec = fusion.getFusionPose();
+      
+      char x = 'x', y = x+1, z = x+2;
+      x -= ID*3;
+      y -= ID*3;
+      z -= ID*3;
+      
+      //x, y, z: roll, pitch, yaw
+ 
+      String line;
+
+      line += "<";
+      line += x;
+      line += ":" ;
+      line += (vec.x() * RTMATH_RAD_TO_DEGREE);
+      line += ">";
+      line += "<";
+      line += y;
+      line += ":" ;
+      line += (vec.y() * RTMATH_RAD_TO_DEGREE);
+      line += ">";
+      line += "<";
+      line += z;
+      line += ":" ;
+      line += (vec.z() * RTMATH_RAD_TO_DEGREE);
+      line += ">";
+      Serial.print(line);
+      
+/*
+      Serial.print( "<");
+      Serial.print( x);
+      Serial.print( ":" );
+      Serial.print( (vec.x() * RTMATH_RAD_TO_DEGREE));
+      Serial.print( ">");
+      Serial.print( "<");
+      Serial.print( y);
+      Serial.print( ":" );
+      Serial.print( (vec.y() * RTMATH_RAD_TO_DEGREE));
+      Serial.print( ">");
+      Serial.print( "<");
+      Serial.print( z);
+      Serial.print( ":" );
+      Serial.print( (vec.z() * RTMATH_RAD_TO_DEGREE));
+      Serial.print( ">");
+*/
+      if(ID==0)
+        ;//Serial.println();//for debug only
+    }
+
+};
--- Sensor2Bluetooth/Sensor2Bluetooth.ino
+++ Sensor2Bluetooth/Sensor2Bluetooth.ino
@@ -1,41 +1,25 @@
 /*
 
-Read sensor data (MPU9250 on I2C ports A4/A5), 
+Read multiple sensor data (MPU9250 on I2C ports A4/A5),
 wrap them, and send to Serial (COM, or Bluetooth on TX/RX).
 
 By Zhaoyang, Jan 2016.
 
 */
 /*
+传感器连接：
+两个传感器 SCL 连在一起，to arduino A5，同时经 10kΩ 上拉电阻 to 3.3V
+两个传感器 SDA 连在一起，to arduino A4，同时经 10kΩ 上拉电阻 to 3.3V
+0 号传感器 AD0 to 3.3V
+1 号传感器 AD0 to GND
+*/
+/*
  * 传感器的放置：
  * 传感器的 xOy 平面与手臂背侧面重合，x 轴指向手指尖；z 轴指向体外。
  */
+
 //uncomment this to get detailed status report on Serial
-//#define VERBOSE
-
-////////////////////////////////////////////////////////////////////////////
-//
-// This file is part of RTIMULib-Arduino
-//
-// Copyright (c) 2014, richards-tech
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy of
-// this software and associated documentation files (the "Software"), to deal in
-// the Software without restriction, including without limitation the rights to use,
-// copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
-// Software, and to permit persons to whom the Software is furnished to do so,
-// subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
+#define VERBOSE
 
 #include <Wire.h>
 #include "I2Cdev.h"
@@ -45,95 +29,36 @@ By Zhaoyang, Jan 2016.
 #include "CalLib.h"
 #include <EEPROM.h>
 
-RTIMU *imu;                      // the IMU object
-RTFusionRTQF fusion;             // the fusion object
-RTIMUSettings settings;          // the settings object
+#include "Sensor.h"
 
-// DISPLAY_INTERVAL sets the rate at which results are displayed
+#define cntSensors 2 // no more than 2
+Sensor sensors[cntSensors];
+
 
 #define DISPLAY_INTERVAL 50      // interval between pose displays
-
-// SERIAL_PORT_SPEED defines the speed to use for the debug serial port
-
 #define SERIAL_PORT_SPEED 9600
 
 unsigned long lastDisplay;
-unsigned long lastRate;
-int sampleCount;
-
-void sendToSerial(RTVector3& vec){
-  String line;
-  //x, y, z: roll, pitch, yaw
-  line += "<x:";
-  line += (vec.x() * RTMATH_RAD_TO_DEGREE);
-  line += ">";
-  line += "<y:" ;
-  line += (vec.y() * RTMATH_RAD_TO_DEGREE);
-  line += ">";
-  line += "<z:" ;
-  line += (vec.z() * RTMATH_RAD_TO_DEGREE);
-  line += ">";
-  Serial.print(line);
-  /*
-  Serial.print("<x:"); Serial.print(vec.x() * RTMATH_RAD_TO_DEGREE);
-  Serial.print("><y:"); Serial.print(vec.y() * RTMATH_RAD_TO_DEGREE);
-  Serial.print("><z:"); Serial.print(vec.z() * RTMATH_RAD_TO_DEGREE);
-  Serial.println(">");
-  */
-}
 
 void setup(){
-  int errcode;
-
   Serial.begin(SERIAL_PORT_SPEED);
   Wire.begin();
-  imu = RTIMU::createIMU(&settings);// create the imu object
-#ifdef VERBOSE
-  Serial.print("ArduinoIMU starting using device "); Serial.println(imu->IMUName());
-#endif
-  if ((errcode = imu->IMUInit()) < 0) {
-#ifdef VERBOSE
-    Serial.print("Failed to init IMU: "); Serial.println(errcode);
-#endif
-  }
-#ifdef VERBOSE
-  if (imu->getCalibrationValid())
-    Serial.println("Using compass calibration");
-  else
-    Serial.println("No valid compass calibration data");
-#endif
-  lastDisplay = lastRate = millis();
-  sampleCount = 0;
-
-  // use of sensors in the fusion algorithm can be controlled here
-  // change any of these to false to disable that sensor
-
-  fusion.setGyroEnable(true);
-  fusion.setAccelEnable(true);
-  fusion.setCompassEnable(true);
+  for(int i=0; i<cntSensors; i++)
+    sensors[i].init(i);
+  lastDisplay = millis();
+  Serial.println("ending setup");
 }
 
 void loop(){
   unsigned long now = millis();
-  unsigned long delta;
-
-  if (imu->IMURead()) { // get the latest data if ready yet
-    fusion.newIMUData(imu->getGyro(), imu->getAccel(), imu->getCompass(), imu->getTimestamp());
-    sampleCount++;
-    if ((delta = now - lastRate) >= 1000) {
-#ifdef VERBOSE
-      Serial.print("Sample rate: "); Serial.print(sampleCount);
-      if (imu->IMUGyroBiasValid())
-        Serial.println(", gyro bias valid");
-      else
-        Serial.println(", calculating gyro bias");
-#endif
-      sampleCount = 0;
-      lastRate = now;
-    }
-    if ((now - lastDisplay) >= DISPLAY_INTERVAL) {
-      lastDisplay = now;
-      sendToSerial((RTVector3&)fusion.getFusionPose()); // formatted output
-    }
+  bool itIsTimeToSendData = (now - lastDisplay) >= DISPLAY_INTERVAL;
+  for(int i=0; i<cntSensors; i++){
+    sensors[i].refresh();
   }
+  if(itIsTimeToSendData){
+    lastDisplay = now;
+    for(int j=0; j<cntSensors; j++)
+       sensors[j].sendToSerial();
+  }
+  
 }
